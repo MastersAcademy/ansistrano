d1dabf3d867b0be2725e156320682eee8ddccb29
yuriirudenko <yurii.rudenko@masterofcode.com>
2019-04-22 21:22:02 +0300

    sh

--- ansible/up_project.sh
+++ ansible/up_project.sh
@@ -2,7 +2,6 @@
 
 docker-compose -f docker-compose.production.yml down
 docker-compose -f docker-compose.production.yml build
-docker-compose -f docker-compose.production.yml run --rm rails rake db:create
 docker-compose -f docker-compose.production.yml run --rm rails rake db:migrate
 docker-compose -f docker-compose.production.yml stop
 docker-compose -f docker-compose.production.yml up -d
